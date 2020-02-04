4f59ee29d5b51fe734a34fe9594a23aadfee7e12
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2020-02-04 21:29:21 +0200

    Add APK validation tests

--- extractor/apk.py
+++ extractor/apk.py
@@ -8,5 +8,15 @@ def extract(zip_path, in_path, out_path):
             f.write(data)
 
 
+def zip_contains(zip_file, name):
+    return any(x == name for x in zip_file.namelist())
+
+
 def is_apk(filename):
-    return zipfile.is_zipfile(filename)
+    if not zipfile.is_zipfile(filename):
+        return False
+    with zipfile.ZipFile(filename, 'r') as z:
+        return all([
+            zip_contains(z, 'classes.dex') and
+            zip_contains(z, 'AndroidManifest.xml')
+        ])
--- tests/test_apk.py
+++ tests/test_apk.py
@@ -1,7 +1,7 @@
 # noinspection PyUnresolvedReferences
 from .fixtures import temporary_path  # noqa: F401
 from .helpers import get_relative_path
-from extractor.apk import extract
+from extractor.apk import extract, is_apk
 
 
 def test_extract(temporary_path):  # noqa: F811
@@ -10,3 +10,13 @@ def test_extract(temporary_path):  # noqa: F811
     extract(zip_path, in_path, temporary_path)
     with open(temporary_path, 'rt') as f:
         assert f.read() == 'This is a test file.'
+
+
+def test_is_apk_succeeds_with_valid_apk():
+    apk_path = get_relative_path('fixtures/test_valid_apk.apk')
+    assert is_apk(apk_path)
+
+
+def test_is_apk_fails_with_invalid_apk():
+    apk_path = get_relative_path('fixtures/test_invalid_apk.apk')
+    assert not is_apk(apk_path)
