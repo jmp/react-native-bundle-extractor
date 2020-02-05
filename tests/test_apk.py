8491d641d8648eb7d591a06c34cf4e0bce9e91b8
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2020-02-05 21:33:58 +0200

    Cover some edge cases with tests

--- tests/test_apk.py
+++ tests/test_apk.py
@@ -1,10 +1,12 @@
+import pytest
+
 # noinspection PyUnresolvedReferences
 from .fixtures import temporary_path  # noqa: F401
 from .helpers import get_relative_path
-from extractor.apk import extract, is_apk
+from extractor.apk import extract, is_apk, BundleNotFoundError
 
 
-def test_extract(temporary_path):  # noqa: F811
+def test_extract_path_exists(temporary_path):  # noqa: F811
     zip_path = get_relative_path('fixtures/test_extract.zip')
     in_path = 'some/directory/test.txt'
     extract(zip_path, in_path, temporary_path)
@@ -12,11 +14,20 @@ def test_extract(temporary_path):  # noqa: F811
         assert f.read() == 'This is a test file.'
 
 
+def test_extract_path_does_not_exist(temporary_path):  # noqa: F811
+    zip_path = get_relative_path('fixtures/test_extract.zip')
+    in_path = 'this/does/not/exist'
+    with pytest.raises(BundleNotFoundError):
+        extract(zip_path, in_path, temporary_path)
+
+
 def test_is_apk_succeeds_with_valid_apk():
-    apk_path = get_relative_path('fixtures/test_valid_apk.apk')
-    assert is_apk(apk_path)
+    assert is_apk(get_relative_path('fixtures/test_valid_apk.apk'))
 
 
 def test_is_apk_fails_with_invalid_apk():
-    apk_path = get_relative_path('fixtures/test_invalid_apk.apk')
-    assert not is_apk(apk_path)
+    assert not is_apk(get_relative_path('fixtures/test_invalid_apk.apk'))
+
+
+def test_is_apk_fails_with_non_zip_file():
+    assert not is_apk(get_relative_path('fixtures/test_non_zip_file.apk'))
