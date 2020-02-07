40627580e296ebde4090c3346f3333b40004b6dd
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2020-02-07 22:24:56 +0200

    Simplify beautify test

--- tests/test_bundle.py
+++ tests/test_bundle.py
@@ -1,13 +1,12 @@
-from .helpers import get_relative_path
 from extractor.bundle import beautify
 
 
-def test_beautify(tmp_path):  # noqa: F811
-    in_path = get_relative_path('fixtures/test_beautify.js')
+def test_beautify(tmp_path):
+    in_path = tmp_path / 'ugly.js'
+    in_path.write_text('const f=()=>{const a=42;return a;};const b=f();')
     out_path = tmp_path / 'beautified.js'
     beautify(in_path, out_path)
-    with out_path.open('rt') as f:
-        assert f.read() == '''
+    assert out_path.read_text() == '''
 const f = () => {
     const a = 42;
     return a;
