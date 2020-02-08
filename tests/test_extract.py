080ca4220d5a71004ace7f97c22bfdc0684dfe2f
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2020-02-08 15:05:45 +0200

    Add test for pull_apk_from_device

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,7 +1,8 @@
 import io
 import zipfile
+from unittest.mock import patch
 
-from extract import extract_bundle_from_apk
+from extract import extract_bundle_from_apk, pull_apk_from_device
 
 
 def test_extract_bundle_from_apk(tmp_path):
@@ -17,3 +18,30 @@ def test_extract_bundle_from_apk(tmp_path):
     extract_bundle_from_apk(apk_path, bundle_in_path, bundle_out_path)
     assert bundle_out_path.exists()
     assert bundle_out_path.read_text() == 'const a = 42;'
+
+
+@patch('extract.check_adb')
+@patch('extract.get_packages')
+@patch('extract.verify_package_exists')
+@patch('extract.find_package_path')
+@patch('extract.pull_path')
+def test_pull_apk_from_device(
+        mock_pull_path,
+        mock_find_package_path,
+        mock_verify_package_exists,
+        mock_get_packages,
+        mock_check_adb,
+):
+    package = 'com.example.app'
+    out_path = '/some/path/app.apk'
+
+    mock_check_adb.return_value = None
+    mock_get_packages.return_value = [package]
+    mock_verify_package_exists.return_value = None
+    mock_find_package_path.return_value = f'/data/app/{package}-RGW6eku5yrzZ' \
+                                          f'062ftW4_7Q==/base.apk'
+    pull_apk_from_device(package, out_path)
+    mock_pull_path.assert_called_with(
+        mock_find_package_path.return_value,
+        out_path,
+    )
