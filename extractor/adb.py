b8b859f1df82664cb726e08dfb63e9df9b2afb3e
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-12-06 16:21:31 +0200

    Hide _execute

--- extractor/adb.py
+++ extractor/adb.py
@@ -7,13 +7,6 @@ from .exceptions import ExecutableNotFoundError, ExecuteError, PackageNotFoundEr
 PACKAGE_PREFIX = "package:"
 
 
-def execute(command):
-    result = subprocess.run(command.split(), capture_output=True)
-    if result.stderr:
-        raise ExecuteError(result.stderr.decode("utf-8"))
-    return result.stdout.decode("utf-8").strip()
-
-
 @log("Checking ADB")
 def check_adb():
     if shutil.which("adb") is None:
@@ -25,22 +18,29 @@ def check_adb():
 
 @log("Listing packages")
 def get_packages():
-    result = execute("adb shell pm list packages")
+    result = _execute("adb shell pm list packages")
     return [line.replace(PACKAGE_PREFIX, "") for line in result.splitlines()]
 
 
 @log("Finding package path")
 def find_package_path(package):
-    result = execute(f"adb shell pm path {package}")
+    result = _execute(f"adb shell pm path {package}")
     return result.replace(PACKAGE_PREFIX, "")
 
 
 @log("Pulling from device")
 def pull_path(path, out_path):
-    return execute(f"adb pull {path} {out_path}")
+    return _execute(f"adb pull {path} {out_path}")
 
 
 @log("Finding package")
 def verify_package_exists(package, packages):
     if package not in packages:
         raise PackageNotFoundError(f'Package "{package}" was not found!')
+
+
+def _execute(command):
+    result = subprocess.run(command.split(), capture_output=True)
+    if result.stderr:
+        raise ExecuteError(result.stderr.decode("utf-8"))
+    return result.stdout.decode("utf-8").strip()
