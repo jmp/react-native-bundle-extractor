aa507b923dfda6b84f0c078897b0fad7f67603c1
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2021-12-06 11:47:45 +0200

    Remove unused function

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,10 +1,3 @@
-import os
-
-
-def get_relative_path(path):
-    return os.path.join(os.path.dirname(__file__), path)
-
-
 class StringContaining(str):
     def __eq__(self, other):
         return self in other
